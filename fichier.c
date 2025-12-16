207f8f77e6b8626955245f90b7005ca548eec2e7
Rafaeldst <rafael.doussaint@gmail.com>
2025-12-16 17:52:08 +0100

    Update fichier.c

--- fichier.c
+++ fichier.c
@@ -12,7 +12,7 @@ void verifier_erreur_fichier(FILE* fichier){//procédure permettant de vérifier
   }
 }
 
-void lecture(const char* nom_fichier, AVL** avl){//procédure qui va lire toutes les lignes du fichier et remplir l'avl
+void lecture(const char* nom_fichier, Usine** usine){//procédure qui va lire toutes les lignes du fichier et remplir l'avl
   FILE* fichier=fopen(nom_fichier,"r");
   verifier_erreur_fichier(fichier);
   char ligne[TAILLE_LIGNE];
@@ -34,20 +34,20 @@ void lecture(const char* nom_fichier, AVL** avl){//procédure qui va lire toutes
       exit(EXIT_FAILURE);
     }
     if(!strcmp(colonne[0],"-") && !strcmp(colonne[2],"-") && !strcmp(colonne[4],"-")){//si la ligne correspond à une usine
-      AVL* est_dans_avl=recherche(*avl,colonne[1]);
+      Usine* est_dans_avl=recherche(*usine,colonne[1]);
       if(est_dans_avl==NULL){//usine pas encore crée
-        AVL* nouveau=creerAVL(colonne[1],atof(colonne[3]);
-        *avl=insertAVL(*avl,nouveau);
+        Usine* nouveau=creerAVL(colonne[1],atof(colonne[3]);
+        *usine=insertAVL(*avl,nouveau);
       }
       else{
         est_dans_avl->capacite_max=atof(colonne[3]);
       }
     }
     else if(!strcmp(colonne[0],"-") && strcmp(colonne[3],"-")){//si la ligne correspond à un captage
-      AVL* est_dans_avl=recherche(*avl,colonne[2]);
+      Usine* est_dans_avl=recherche(*avl,colonne[2]);
       if(est_dans_avl==NULL){//usine pas encore créer
-        AVL* nouveau=creerAVL(colonne[2],0);
-        *avl=insertAVL(*avl,nouveau);
+        Usine* nouveau=creerAVL(colonne[2],0);
+        *usine=insertAVL(*avl,nouveau);
         nouveau->volume_total_capte=nouveau->volume_total_capte+atof(colonne[3]);
         nouveau->volume_total_traite=nouveau->volume_total_traite+atof(colonne[3])*(1-atof(colonne[4])/100);
       }
@@ -60,6 +60,6 @@ void lecture(const char* nom_fichier, AVL** avl){//procédure qui va lire toutes
   fclose(fichier);
 }
 
-void ecrire(Avl* avl,const char* nom_fichier,const char* mode){
+void ecrire(Usine* usine,const char* nom_fichier,const char* mode){
 
 }
