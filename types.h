b4ab1db7546aa7325a0d657983055c4f7e0070ab
Rafaeldst <rafael.doussaint@gmail.com>
2025-12-19 19:15:09 +0100

    Add types.h with data structures and function prototypes

---
+++ types.h
@@ -0,0 +1,52 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Lien {
+    struct Noeud *enfant;
+    struct Lien *suivant;
+} Lien;
+
+typedef struct Usine {
+    char *id;
+    long capa_max;
+    long total_capte;
+    long total_traite;
+    
+    int equilibre;          // Changement de nom (ex-hauteur) pour ton AVL
+    struct Usine *gauche;
+    struct Usine *droite;
+    
+    Lien *stockages_initiaux; // Gardé pour la liaison au réseau
+} Usine;
+
+typedef struct Noeud {
+    char *id;
+    float pourcentage_fuite;
+    int nb_enfants;
+    int est_libere;
+    Lien *liste_enfants;    // La liste chaînée du graphe
+    int equilibre;          // Changement de nom pour ton AVL de recherche
+    struct Noeud *avl_gauche;
+    struct Noeud *avl_droite;
+} Noeud;
+
+
+int max2(int a, int b);
+int min2(int a, int b);
+int max3(int a, int b, int c);
+int min3(int a, int b, int c);
+Usine* inserer_usine(Usine *a, Usine *nouvelle, int *h);
+Noeud* inserer_noeud_avl(Noeud *a, Noeud *nouv, int *h);
+void liberer_arbre_usine(Usine *a);
+void liberer_graphe(Noeud *racine);
+
+// Fonctions que tes potes doivent coder (on les laisse ici pour le main)
+int traiter_donnees_histo(const char *fichier, Usine **racine);
+void ecrire_donnees_histo(Usine *racine, const char *fichier, const char *type);
+float calculer_fuites(const char *fichier, const char *id_usine);
+
+#endif
